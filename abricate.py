496a0be29cc6dac5e61b1c10d8527c519d986ffc
martind <javier.martin.benito@alumnos.uva.es>
2021-07-07 11:32:20 +0200

    author and description added

--- abricate.py
+++ abricate.py
@@ -1,2 +1,16 @@
 import numpy as np
-print("hola, mundo")
+import pandas as pd
+
+"""
+Script made by Javier Martín de Benito while businesss practicing at Instituto de
+Tecnológico Agrario de Castilla y León (Valladolid, Castile and Leon).
+
+This Python script is based on the Abricate Perl program, made by Torsten Seemann.
+"""
+
+__author__ = "Javier Martín"
+__url__ = "https://github.com/javiermdb99"
+
+columns = ("FILE", "SEQUENCE", "START", "END", "STRAND", "GENE", "COVERAGE",
+           "COVERAGE_MAP", "GAPS", "%COVERAGE", "%IDENTITY", "DATABASE", "ACCESSION",
+           "PRODUCT", "RESISTANCE")
